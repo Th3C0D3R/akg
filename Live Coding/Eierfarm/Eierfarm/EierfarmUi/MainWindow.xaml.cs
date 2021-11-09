1a4f8c3c186d50b6caee4a4726a10e5c79fa133d
Olaf Lischke <olaf.lischke@lischke-edv.de>
2021-11-09 08:39:59 +0100

    Live Coding Tag 1

---
+++ Live Coding/Eierfarm/Eierfarm/EierfarmUi/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using EierfarmBl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EierfarmUi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNeuesHuhn_Click(object sender, RoutedEventArgs e)
+        {
+            Huhn huhn = new Huhn(); //{ Name = "Neues Huhn" };
+
+            cbxTiere.Items.Add(huhn);
+            cbxTiere.SelectedItem = huhn;
+        }
+
+        private void btnNeueGans_Click(object sender, RoutedEventArgs e)
+        {
+            Gans gans = new Gans();
+
+            cbxTiere.Items.Add(gans);
+            cbxTiere.SelectedItem = gans;
+        }
+
+        private void btnEiLegen_Click(object sender, RoutedEventArgs e)
+        {
+            if (cbxTiere.SelectedItem is Gefluegel tier)
+            {
+                tier.EiLegen();
+            }
+        }
+
+        private void btnFuettern_Click(object sender, RoutedEventArgs e)
+        {
+            Gefluegel tier = cbxTiere.SelectedItem as Gefluegel;
+
+            if (tier!=null)
+            {
+                tier.Fressen();
+            }
+        }
+    }
+}
---
+++ Live Coding/Eierfarm/EierfarmBl/Gefluegel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EierfarmBl
+{
+    public abstract class Gefluegel
+    {
+
+        public Gefluegel(string name)
+        {
+            this.Name = name;
+        }
+
+        public double Gewicht { get; set; }
+        public string Name { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public List<Ei> Eier { get; set; } = new List<Ei>();
+
+        public abstract void Fressen();
+        public abstract void EiLegen();
+    }
+
+}
